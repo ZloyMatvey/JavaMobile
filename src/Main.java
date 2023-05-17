f8585cabfd0d5b354131b7dc5c7c418ea364ed6e
ZloyMatvey <122729209+ZloyMatvey@users.noreply.github.com>
2023-05-17 16:59:33 +0300

    correct

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         int balans = 200; // Баланс клиента
         int sum = 5000; // Сумма пополнения
-        int a = 100; // 100 рублей за которые начисляется 1 рубль
+        int onebonus = 100; // 100 рублей за которые начисляется 1 рубль
         if (sum <= 1000) {
             System.out.println("Ваш баланс = " + (balans + sum));
         } else {
-            int bonus = sum / a;
+            int bonus = sum / onebonus;
             System.out.println("Поздравляю! вы получили " + bonus + " бонусов");
             System.out.println("Ваш баланс = " + (balans + sum + bonus));
         }

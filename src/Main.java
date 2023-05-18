7913bf4021666f070df53cab0d478dc89f941122
ZloyMatvey <122729209+ZloyMatvey@users.noreply.github.com>
2023-05-18 15:10:07 +0300

    oneBonus

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
     public static void main(String[] args) {
         int balans = 200; // Баланс клиента
         int sum = 5000; // Сумма пополнения
-        int onebonus = 100; // 100 рублей за которые начисляется 1 рубль
+        int oneBonus = 100; // 100 рублей за которые начисляется 1 рубль
         if (sum <= 1000) {
             System.out.println("Ваш баланс = " + (balans + sum));
         } else {
-            int bonus = sum / onebonus;
+            int bonus = sum / oneBonus;
             System.out.println("Поздравляю! вы получили " + bonus + " бонусов");
             System.out.println("Ваш баланс = " + (balans + sum + bonus));
         }
